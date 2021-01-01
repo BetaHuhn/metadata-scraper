31581f387baf6c8dd682e9f3a0224da6bbeadf62
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2021-01-01 13:07:56 +0100

    🚨 Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,18 @@
-module.exports = require('@betahuhn/config').eslint
+module.exports = {
+	...require('@betahuhn/config').eslint,
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: [
+		'@typescript-eslint'
+	],
+	extends: [
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	rules: {
+		'func-style': 0,
+		'@typescript-eslint/no-explicit-any': 0,
+		'@typescript-eslint/explicit-module-boundary-types': 0,
+		'@typescript-eslint/no-var-requires': 0
+	}
+}
